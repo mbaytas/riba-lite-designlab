6ee22a62d50c2e8449cddc99723d4c9b0ac2abf2
Mehmet Aydın Baytaş <mbaytas@Lumos-III.local>
2015-10-14 16:03:43 +0300

    v1.0.0

--- header.php
+++ header.php
@@ -1,19 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
-
-<?php
-
-    $riba_lite_logo_text = get_theme_mod('riba_lite_text_logo', __('Riba', 'riba-lite') );
-?>
-
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
@@ -21,19 +15,17 @@
 
     <?php do_action('mtl_site_preloader'); ?>
 
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'riba-lite' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'riba-lite' ); ?></a>
 
     <?php do_action('mtl_before_header'); ?>
 
 
         <div id="header-container" class="container-fluid">
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding col-lg-4 col-md-4 col-sm-4 col-xs-12 text-lg-left text-md-left text-sm-left text-xs-center">
-					<?php
-                        echo '<a class="rl-text-logo" href="'.esc_url( home_url( '/' ) ).'" rel="home" >'.esc_attr( $riba_lite_logo_text ).'</a>';
-                    ?>
-				</div><!-- .site-branding -->
+            <header id="masthead" class="site-header" role="banner">
+                <div class="site-branding col-lg-4 col-md-4 col-sm-4 col-xs-12 text-lg-left text-md-left text-sm-left text-xs-center">
+                    <a href="<?php home_url() ?>" rel="home" ><img src="<?php bloginfo('stylesheet_directory')?>/images/logo-designlab.png"></a>
+                </div><!-- .site-branding -->
 
 
     <nav id="site-navigation" class="main-navigation col-lg-8 col-md-8 col-sm-8 hidden-xs" role="navigation">
@@ -50,5 +42,5 @@
 </div>
 
 
-	<div id="content" class="site-content container-fluid">
+    <div id="content" class="site-content container-fluid">
 
